3ae0d61da3c3f5baae4c741945189a12a207692c
Tanjong <tanjong.njie@outlook>
2022-08-18 03:31:39 -0500

    Adding config file

--- config.tf
+++ config.tf
@@ -13,3 +13,10 @@ provider "azurerm" {
   features {}
 }
 
+resource "azurerm_container_registry" "demo-app" {
+  name                     = "demoapp${random_string.random-name.result}"
+  resource_group_name      = azurerm_resource_group.demo.name
+  location                 = azurerm_resource_group.demo.location
+  sku                      = "Standard"
+  admin_enabled            = false
+}
