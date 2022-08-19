8a2571a3fd1eeeecb9bf6a72f1baa2525ddefed8
Tanjong <tanjong.njie@outlook>
2022-08-19 11:21:36 -0500

    Adding config file

--- acr.tf
+++ acr.tf
@@ -4,16 +4,16 @@ resource "azurerm_resource_group" "devlab-democluster-acr-rg" {
   location = local.buildregion
 }
 
-resource "random_string" "random-name" {
-  length  = 8
-  upper   = false
-  lower   = false
-  numeric  = false
-  special = false
+resource "random_string" "random" {
+  length           = 5
+  special          = false
+  lower            = true
+  upper            = "false"
+  override_special = "!@#$%&*()-_=+[]{}<>:?"
 }
 
 resource "azurerm_container_registry" "demo-app" {
-  name                     = "demoapp${random_string.random-name.result}"
+  name                     = "demoapp${random_string.random.result}"
   resource_group_name      = azurerm_resource_group.devlab-democluster-acr-rg.name
   location                 = azurerm_resource_group.devlab-democluster-acr-rg.location
   sku                      = "Standard"
--- config.tf
+++ config.tf
@@ -4,8 +4,15 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "3.10.0"
     }
+
+      random = {
+      source  = "hashicorp/random"
+      version = "3.3.2"
+    }
   }
+  required_version = ">= 0.13"
 }
+  
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
